bd44c3d17a88273b390caca70bee7d555365b409
boos4721 <3.1415926535boos@gmail.com>
2022-02-23 15:48:59 +0800

    luci: fix showing bug
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+++ applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
@@ -5,5 +5,8 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "cpufreq"}, cbi("cpufreq"), _("CPU Freq"), 900).dependent = false
+	local page
+	page = entry({"admin", "system", "cpufreq"}, cbi("cpufreq"), _("CPU Freq"), 900)
+	page.dependent = false
+	page.acl_depends = { "luci-app-cpufreq" }
 end
